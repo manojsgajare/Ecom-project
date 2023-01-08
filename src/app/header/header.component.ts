91ea8faa16aa57b019e91754bf44a44bbbc480e1
manoj <98097534+manojsgajare@users.noreply.github.com>
2023-01-08 19:24:54 +0530

    productForm

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  menuType: String = 'default';
+  menuType: string = 'default';
+  sellerName: string = '';
+
   constructor(private route: Router) {}
 
   ngOnInit(): void {
@@ -16,11 +18,22 @@ export class HeaderComponent implements OnInit {
         if (localStorage.getItem('seller') && val.url.includes('seller')) {
           console.warn('in seller area');
           this.menuType = 'seller';
+
+          if (localStorage.getItem('seller')) {
+            let sellerStore = localStorage.getItem('seller');
+            let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+            this.sellerName = sellerData.name;
+          }
         } else {
           console.warn('Outside seller');
           this.menuType = 'default';
         }
       }
     });
+  }
+
+  logout() {
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
   }
 }
